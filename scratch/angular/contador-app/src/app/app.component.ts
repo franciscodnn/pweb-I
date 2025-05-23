5c636d414d8ed0e96943684eab0edf92941736d4
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2025-05-23 09:06:51 -0300

    Aula de Templates Angular

---
+++ scratch/angular/contador-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'contador-app';
+
+  imagem = 'https://www.ifpb.edu.br/imagens/logotipos/ifpb-1';
+}
